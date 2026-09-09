2511a728fdfb579577ab29e4c205f6110df2ad02
Student <Student@LabVM2049939.jv03jwyx44iuve4cb404kghoog.jnbx.internal.cloudapp.net>
2026-09-10 00:10:15 +0200

    Add Login class and Main class into my source package Add private fields for login.jave Created Login constructor for logging user Added username validation method Added password complexity validation

--- src/main/java/com/mycompany/prog5121poe/Login.java
+++ src/main/java/com/mycompany/prog5121poe/Login.java
@@ -9,5 +9,26 @@ package com.mycompany.prog5121poe;
  * @author Student
  */
 public class Login {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+
+public Login(String firstname, String lastName, String username, String password, String cellPhoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+    this.username = username;
+    this.cellPhoneNumber = cellPhoneNumber;
+}
+public boolean checkUserName() {
+    return username.contains("_") && username.length() <=5;
     
+public boolean.checkPasswordComplexity() {
+    return password() >= 8
+            && password,matches(".*[A-Z].*")
+            && password,matches(.*\\d.*)
+            && password,matches(".*[^a-zA-Z0-9].*");
+}
 }
