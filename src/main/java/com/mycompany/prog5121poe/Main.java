51f983629fe9f3c57a40c674c615df99e777a45c
Student <Student@LabVM2049939.jv03jwyx44iuve4cb404kghoog.jnbx.internal.cloudapp.net>
2026-09-16 19:24:51 +0200

    Add Login class and Main class into my source package Add private fields for login.jave Created Login constructor for logging user Added username validation method Added password complexity validation Fixed syntax errors Added Cell Phone validation Added registerUser method i implemented loginUser method I implemented login status method I added registration test to main program Added login Functionality test Fixed login verification functionality added Scanner for main program Fixed login verification functionality added registration head and first and input,last name input and username

--- src/main/java/com/mycompany/prog5121poe/Main.java
+++ src/main/java/com/mycompany/prog5121poe/Main.java
@@ -9,5 +9,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("================================");
+        System.out.println("QUICKCHAT REGISTRATION");
+        System.out.println("================================");
+        
+        System.out.print("Enter your first name; ");
+        String firstName =scanner.nextLine();
+        
+        System.out.print("Enter your last name; ");
+        String lastName =scanner.nextLine();
+        
+        System.out.print("Enter your username; ");
+        String username =scanner.nextLine();
     }
-}
+    }
