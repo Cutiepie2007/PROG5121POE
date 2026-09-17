6e2aff2213885b3cc052a9151eea1bde03d237ea
Student <Student@LabVM2049939.jv03jwyx44iuve4cb404kghoog.jnbx.internal.cloudapp.net>
2026-09-17 17:49:55 +0200

    Add Login class and Main class into my source package Add private fields for login.jave Created Login constructor for logging user Added username validation method Added password complexity validation Fixed syntax errors Added Cell Phone validation Added registerUser method i implemented loginUser method I implemented login status method I added registration test to main program Added login Functionality test Fixed login verification functionality added Scanner for main program Fixed login verification functionality added registration head and first and input,last name input and username added password input and cell phone number input created login object in main I added username,password,cellphone number validation and registration results added Junit test class and i added test username validation and i added junit test for password validation added Junit test for password and registration validation

--- src/test/java/com/mycompany/prog5121poe/LoginTest.java
+++ src/test/java/com/mycompany/prog5121poe/LoginTest.java
@@ -27,4 +27,32 @@ public class LoginTest {
         
         assertTrue(user.checkPasswordComplexity());
     }
+    
+    @Test
+    public void testCheckCellPhoneNumber(){
+        Login user = new Login(
+                "John",
+                "Smith",
+                "kyl_1",
+                "Ch&&sec@ke99!",
+                "+27838668976"
+        );
+        
+        assertTrue(user.checkCellPhoneNumber());
+    }
+    
+    @Test
+    public void testRegistrationResult(){
+        Login user = new Login(
+                "John",
+                "Smith",
+                "kyl_1",
+                "Ch&&sec@ke99!",
+                "+27838668976"
+        );
+        
+        assertTrue(user.checkUserName());
+        assertTrue(user.checkPasswordComplexity());
+        assertTrue(user.checkCellPhoneNumber());
+    }
 }
