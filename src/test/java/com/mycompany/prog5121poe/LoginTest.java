f99e48932c9e4267cb27ecf462fea209f8c7783b
Student <Student@LabVM2049939.jv03jwyx44iuve4cb404kghoog.jnbx.internal.cloudapp.net>
2026-09-17 22:16:16 +0200

    Add Login class and Main class into my source package Add private fields for login.jave Created Login constructor for logging user Added username validation method Added password complexity validation Fixed syntax errors Added Cell Phone validation Added registerUser method i implemented loginUser method I implemented login status method I added registration test to main program Added login Functionality test Fixed login verification functionality added Scanner for main program Fixed login verification functionality added registration head and first and input,last name input and username added password input and cell phone number input created login object in main I added username,password,cellphone number validation and registration results added Junit test class and i added test username validation and i added junit test for password validation added Junit test for password and registration validation added Junit test for invalid username and added Junit test for invalid password

--- src/test/java/com/mycompany/prog5121poe/LoginTest.java
+++ src/test/java/com/mycompany/prog5121poe/LoginTest.java
@@ -1,7 +1,8 @@
 package com.mycompany.prog5121poe;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 public class LoginTest {
     
     @Test
@@ -54,5 +55,31 @@ public class LoginTest {
         assertTrue(user.checkUserName());
         assertTrue(user.checkPasswordComplexity());
         assertTrue(user.checkCellPhoneNumber());
+    }
+    
+    @Test
+    public void testInvalidUserName(){
+        Login user = new Login(
+                "John",
+                "Smith",
+                "kyl1",
+                "Ch&&sec@ke99!",
+                "+27838968976"
+        );
+        
+        assertFalse(user.checkUserName());
+    }
+    
+    @Test
+    public void testInvalidPassword(){
+        Login user = new Login(
+                "John",
+                "Smith",
+                "kyl_1",
+                "password",
+                "+2783896879"
+        );
+        
+        assertFalse(user.checkPasswordComplexity());
     }
 }
